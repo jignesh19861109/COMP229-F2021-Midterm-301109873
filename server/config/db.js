cb6adb385bfc3e3f464b6ce91c13542d6e07bbf7
Jignesh Patel <jignesh.bit@gmail.com>
2021-10-28 05:00:37 -0400

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/book_store"
+  "URI": "mongodb+srv://JigneshPatel:Jigs1991..@cluster09.wmnrq.mongodb.net/portfolio_database?retryWrites=true&w=majority",
+
+};
